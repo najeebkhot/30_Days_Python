a6a5bbc3c616b5086af3b2cb4a442c180fe2e94c
najeebkhot <najeebkhot.1@gmail.com>
2023-12-06 23:27:53 +0530

    Added Day 01 file.

--- Day_01/day_01.py
+++ Day_01/day_01.py
@@ -12,11 +12,11 @@ print(3 // 2)  # Floor division operator(//)
 
 # Checking data types
 
-print(type(10))                  # Int
+print(type(10))                  # Int 
 print(type(3.14))                # Float
 print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
+print(type('John'))              # String
 print(type([1, 2, 3]))           # List
-print(type({'name':'Asabeneh'})) # Dictionary
+print(type({'name':'John'}))     # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set 
 print(type((9.8, 3.14, 2.7)))    # Tuple
