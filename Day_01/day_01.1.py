7558d74f4aacb92a06b7b30944e1999163a46198
najeebkhot <najeebkhot.1@gmail.com>
2023-12-07 00:09:17 +0530

    Added file for Day 01

---
+++ Day_01/day_01.1.py
@@ -0,0 +1,9 @@
+# Getting the data type of the output
+
+print(type(2 + 3))   # addition(+)
+print(type(3 - 1))   # subtraction(-)
+print(type(2 * 3))   # multiplication(*)
+print(type(3 / 2))   # division(/)
+print(type(3 ** 2))  # exponential(**)
+print(type(3 % 2))   # modulus(%)
+print(type(3 // 2))  # Floor division operator(//)
